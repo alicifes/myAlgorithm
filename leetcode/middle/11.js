0155ae23c0f8c49a13fc18d3ee4a5411ca9fc7c9
alicifes <635023574@qq.com>
2023-05-29 17:32:21 +0800

    auto push at 2023-05-29 17:32:21

---
+++ leetcode/middle/11.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} height
+ * @returns {number}
+ * @description 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+ * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ * 返回容器可以储存的最大水量。
+ * @TestCase [1,8,6,2,5,4,8,3,7] => 49   输入：height = [1,1]输出：1
+ */
+
+const maxArea = function (height = []) {
+    const endMaxAreaList = []
+    for(let i = 0;i < height.length; i++){
+        let currMax = 0
+        for(let j = 1; j< height.length;j++ ){
+        }
+    }
+}
