c810894de7a06ec760bfeeee39d3792c9eedf694
alicifes <635023574@qq.com>
2023-07-03 20:43:46 +0800

    auto push at 2023-07-03 20:43:46

--- leetcode/middle/90.js
+++ leetcode/middle/90.js
@@ -5,5 +5,24 @@
  * 解集 不能包含重复的子集。返回的解集中，子集可以按 任意顺序 排列。
  * @testCase [1,2,2] => [[],[1],[1,2],[1,2,2],[2],[2,2]]
  * @testCase [0] => [[],[0]]
- * @testCase [4,4,4,1,4] => [[],[1],[1,4],[1,4,4],[1,4,4,4],[1,4,4,4,4],[4],[4,4],[4,4,4],[4,4,4,4]]
+ * @testCase [4,4,4,1,4] => [[],[1,[1,4],[1,4,4],[1,4,4,4],[1,4,4,4,4],[4],[4,4],[4,4,4],[4,4,4,4]]
  */
+
+const subsetsWithDup = function (nums = []) {
+  nums = nums.sort((a, b) => a - b);
+  const end = [];
+  const router = [];
+  const trackBack = (cur) => {
+    end.push([...router]);
+    if (router.length === cur.length) return;
+    for (let i = cur; i < nums.length; i++) {
+      router.push(nums[cur]);
+      trackBack(i + 1);
+      router.pop();
+    }
+  };
+  trackBack(0);
+  return end;
+};
+
+console.log(subsetsWithDup([4, 4, 4, 1, 4]));
