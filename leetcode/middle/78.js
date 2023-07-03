78d708e17ff4bf8d2026517cf3c7a47d04d357ff
alicifes <635023574@qq.com>
2023-07-03 20:25:58 +0800

    auto push at 2023-07-03 20:25:58

--- leetcode/middle/78.js
+++ leetcode/middle/78.js
@@ -8,4 +8,21 @@
  * @testCase [1,2,3,4] => [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3],[4],[1,4],[2,4],[1,2,4],[3,4],[1,3,4],[2,3,4],[1,2,3,4]]
  */
 
-const subsets = function (nums = []) {};
+const subsets = function (nums = []) {
+  const router = [];
+  const end = [];
+  const trackBack = (cur) => {
+    end.push([...router]);
+    if (router.length === nums.length) return;
+    for (let i = cur; i < nums.length; i++) {
+      router.push(nums[i]);
+      trackBack(i + 1);
+      router.pop();
+    }
+  };
+  trackBack(0)
+  return end
+};
+console.log(subsets([1,2,3]));
+console.log(subsets([1,2,3,4]));
+
