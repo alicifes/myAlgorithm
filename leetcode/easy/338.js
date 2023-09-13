749e6c2287ab85f482bfa0f857ce810a24101f9a
alicifes <635023574@qq.com>
2023-09-13 15:29:09 +0800

    auto push at 2023-09-13 15:29:09

---
+++ leetcode/easy/338.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ * @description 给你一个整数 n ，对于 0 <= i <= n 中的每个 i ，计算其二进制表示中 1 的个数 ，返回一个长度为 n + 1 的数组 ans 作为答案。
+ * @testCase 输入：n = 2 输出：[0,1,1] 解释：0 --> 0 1 --> 1 2 --> 10
+ * @testCase  输入：n = 5 输出：[0,1,1,2,1,2] 解释：0 --> 0 1 --> 1 2 --> 10 3 --> 11 4 --> 100 5 --> 101
+ */
+const countBits = (n = 1) => {
+  let end = parseInt(n, 2);
+  console.log(end);
+};
+
+console.log(countBits(5));
